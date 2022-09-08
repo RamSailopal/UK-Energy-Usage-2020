7c08b7212d90c7c3f29130ee2cb53a3b76fdc779
Raman Sailopal <rsailopal@robinsonbrothers.co.uk>
2022-09-08 16:10:15 +0100

    Allowing cross origin headers

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -2,9 +2,11 @@
 cp /home/yotta/gas.m /opt/mgweb/m 
 cp /home/yotta/elec.m /opt/mgweb/m
 cp /home/yotta/pcode.m /opt/mgweb/m
+cp -f /home/yotta/_zmgweb.m /opt/mgweb/m
 ydb <<< 'ZL "pcode.m"'
 ydb <<< 'ZL "gas.m"'
 ydb <<< 'ZL "elec.m"'
+ydb <<< 'ZL "_zmgweb.m"'
 ydb <<< 'D START^pcode'
 ydb <<< 'D START1^pcode'
 ydb <<< 'D START^gas'
